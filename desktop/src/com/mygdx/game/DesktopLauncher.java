813dd13c4cb2341df0fb411544cd853f28f3b05a
KorViktor1122 <59253167+KorViktor1122@users.noreply.github.com>
2020-05-11 13:35:14 +0300

    Lesson№3 first commit path2

---
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 400;
+        config.height = 600;
+        config.resizable = false;
+        new LwjglApplication(new FutureGame(), config);
+    }
+}
