29f50b3886016ba69e12418613fa283f26b7976d
KorViktor1122 <59253167+KorViktor1122@users.noreply.github.com>
2020-05-11 13:19:52 +0300

    Lesson№3 first commit

--- core/src/com/mygdx/game/Screen/MainScreen.java
+++ core/src/com/mygdx/game/Screen/MainScreen.java
@@ -1,58 +1,43 @@
 package com.mygdx.game.Screen;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector2;
-import com.mygdx.game.Base.BaseScreen;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.Base.BaseScreen;
+import com.mygdx.game.Spirite.Background;
+import com.mygdx.game.math.Rect;
 public class MainScreen extends BaseScreen {
 
+
     private Texture img;
-
-    private static final float V_Len = 0.8f;
-
-    private Vector2 starPossion;
-    private Vector2 choosePoint;
-    private Vector2 touch;
-    private Vector2 common;
+    private Texture bg;
+    private Background background;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("mavr-2.jpg");
-        starPossion = new Vector2();
-        choosePoint = new Vector2();
-        touch = new Vector2();
-        common = new Vector2();
+        img = new Texture("SpaceWall.jpg");
+        bg = new Texture("bg.png");
+        background = new Background(bg);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        common.set(touch);
-        if(common.sub(starPossion).len() > V_Len) {
-            starPossion.add(choosePoint);
-        } else {
-            starPossion.set(touch);
-            choosePoint.setZero();
-        }
         batch.begin();
-        batch.draw(img, starPossion.x, starPossion.y);
+        background.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose() {
         img.dispose();
+        bg.dispose();
         super.dispose();
     }
 
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight()-screenY);
-        System.out.println("touch X = " + touch.x + " touch Y = " + touch.y);
-        choosePoint.set(touch.cpy().sub(starPossion));
-        choosePoint.setLength(V_Len);
-        return super.touchDown(screenX, screenY, pointer, button);
-    }
 }
